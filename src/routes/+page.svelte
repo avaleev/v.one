9eeb469f9f5170de2092e89f58ac66c8e3fb8f1a
Anton Valeev <anton@temerityanalytics.com>
2026-02-12 19:55:38 -0500

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>√𝔳.𝔬𝔫𝔢</title>
+</svelte:head>
+
+<script lang="ts">
+  import avw from '$lib/assets/avw.svg';
+</script>
+
+<div class="h-dvh w-dvw">
+  <img src={avw} alt="AV" height="52" class="h-30 m-auto relative top-[50%] -translate-y-[50%]" />
+</div>
